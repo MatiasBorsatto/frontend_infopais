17e335371d477f54054c194dca2a94aff1a65631
Matias <matiasborsatto1@gmail.com>
2025-10-25 00:23:50 -0300

    Comienzo frontend infopais + router + navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <div class="navbar-1">
+            <div>secciones</div>
+            <div>viernes, Octubre 5, 2025</div>
+            <div class="navbar-1-derecha">
+                <div>buscar</div>
+                <div>Log In</div>
+            </div>
+        </div>
+        <div class="navbar-2">
+            <a href="index.html">
+                <img src="../assets/logo_infopais.png" alt="Imagen Logo">
+            </a>
+        </div>
+        <div class="navbar-3">
+            <router-link to="/mundo">Mundo</router-link>
+            <a href="#">Politica</a>
+            <a href="#">Negocios</a>
+            <a href="#">Opinion</a>
+            <a href="#">Tecnologia</a>
+            <a href="#">Ciencia</a>
+            <a href="#">Salud</a>
+            <a href="#">Deportes</a>
+            <a href="#">Entretenimiento</a>
+            <a href="#">Viajes</a>
+            <a href="#">Mas</a>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.navbar-1 {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    border-bottom: 1px solid var(--borde-suave);
+    padding: .4rem 0 .4rem 0;
+}
+
+.navbar-1-derecha {
+    display: flex;
+    gap: 1rem;
+}
+
+.navbar-2 {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}
+
+.navbar-2 img {
+    width: 15rem;
+    margin: 1rem 0rem 1rem 0rem;
+}
+
+.navbar-3 {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+    margin: 0rem 0rem 1rem 0rem;
+    padding: 5px;
+    border-top: 1px solid var(--borde-suave);
+    border-bottom: 1px solid var(--borde-suave);
+}
+
+.navbar-3 a {
+    text-decoration: none;
+    color: black;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Inicio from '../views/Inicio/Inicio.vue'
+
+// Definís las rutas de tu aplicación
+const routes = [
+  {
+    path: '/',             // URL
+    name: 'inicio',          // Nombre de la ruta
+    component: Inicio    // Vista que se mostrará
+  },
+]
+
+// Creás la instancia del router
+const router = createRouter({
+  history: createWebHistory(), // Usa el modo "histórico" (sin # en la URL)
+  routes                        // Array de rutas
+})
+
+// Exportás para usarlo en main.js
+export default router
